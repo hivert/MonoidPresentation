fe748ebb27bcc6009b21ed41f3161d85e02939de
Assia Mahboubi <assia.mahboubi@inria.fr>
2025-01-13 15:55:43 +0100

    starting sample entry

--- theories/entry_sample.v
+++ theories/entry_sample.v
@@ -1,7 +1,6 @@
 From HB Require Import structures.
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
-From mathcomp Require Import choice bigop fintype finfun finset ssralg.
-
+From mathcomp Require Import all_ssreflect all_algebra.
+Require Import monoids present.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -15,12 +14,13 @@ Unset Printing Implicit Defensive.
 (* Presentation entry, in this case <a, b | aaba = baa>. 
 
 In the current, unsatisfactory state, a presentation is just a rewrite system.
- Later, we should also provide the list of generators. *)
-Definition entry := [:: ([:: 0; 0; 1; 1], [:: 1; 0; 0])].
+Generators are inferred from the letters involved in the system.
+Later, we should also provide the list of generators. *)
+Definition present_entry := [:: ([:: 3; 3; 4; 4], [:: 4; 3; 3])].
 
 
-
-Definition present_final :=
+(* First candidate alternate presentation, this time with five generators *)
+Definition present_final_1 :=
   [:: (*  c < e < d < a < b. *)
       (*  0 < 1 < 2 < 3 < 4. *)
      ([:: 3; 4], [:: 0]);           (* ab → c *)
@@ -37,10 +37,46 @@ Definition present_final :=
      ([:: 3; 1; 1], [:: 0; 3; 1])   (* aee → cae*)
    ].
 
-Theorem final_ok : convergent present_final.
+(* Proof that the entry and final presentations define the same monoid. 
+   Warning: this is an effective result, containing the data of the isomorphism,
+   hence the Defined. *)
+
+Parameter cool : False.
+Theorem present_equiv :  isopres present_entry present_final_1.
+Proof. elim cool. Defined.
+
+Corollary equiv_equal u v : 
+(u = v %[mod present_entry]) <-> (present_equiv u = present_equiv v %[mod present_final_1]).
+Proof. admit. Admitted.
+
+(* Proof that the presentation is terminating + confluent. *)
+Theorem final_1_ok : convergent present_final_1.
 Proof. exact: (check_convergence_natP (fuel := 5)). Qed.
 
+(* The word problem is hence decidable in this monoid. *)
+Theorem decidable_entry : True.
+Proof. done. Qed. 
 
+(* To be implemented combining norfuel and termination somehow *)
+Parameter normalize : seq nat -> seq nat.
+
+Definition test_eq_entry u v := 
+    (normalize (present_equiv u)) == normalize (present_equiv v).
+
+(* And now we provide a formally verified equality test for this monoid. *)
+Theorem test_eqP u v :
+reflect (u = v %[mod present_entry]) (test_eq_entry u v).
+Proof. admit. Admitted.
+
+(* Now users can test (dis)equalities and get a formal proof of the result
+ *)
+
+
+ (* And now an optimized variant: implement a test_eq_opt and prove that it is 
+   pointwise equal to tes_eq *)
+
+
+(***** END ******)   
 
 Goal ([:: 1; 2; 2] < [:: 2; 2; 1])%O. by []. Qed.
 Goal ~~ ([:: 2; 2] < [:: 1])%O. by []. Qed.
