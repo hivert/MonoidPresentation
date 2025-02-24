7cc0c8054bad0e58fe7bf177ab499758ac6d8c88
Florent Hivert <Florent.Hivert@lri.fr>
2025-02-24 15:31:49 +0100

    Adaptator for Tietze 1

--- theories/present.v
+++ theories/present.v
@@ -954,9 +954,14 @@ Definition isopres_rcons_rule := @isopres_eq _ _ rcons_ext_pres equiv_rcons_rule
 End Tietze1.
 
 Lemma Tietze_add_rel  A (R1 R2 : pres A) (u v : word A) :
-  all (mem (pgen R1)) u -> all (mem (pgen R1)) v -> 
-  pgen R1 = pgen R2 -> prelat R2 = rcons (prelat R1) (u, v) -> isopres R1 R2.
-Admitted.
+  all (mem (pgen R1)) u -> all (mem (pgen R1)) v ->
+  pgen R1 = pgen R2 -> prelat R2 = rcons (prelat R1) (u, v) ->
+  u = v %[mod prelat R1] -> isopres R1 R2.
+Proof.
+move=> allu allv eqgen eqrelat newrelat.
+apply: (isopres_trans (isopres_rcons_rule allu allv newrelat)).
+exact: pres_irrelevance.
+Qed.
 
 
 Section Tietze2.
