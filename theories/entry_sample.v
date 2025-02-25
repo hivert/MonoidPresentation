153e154f5225bf9babff2afae8f273b34e02674e
Florent Hivert <Florent.Hivert@lri.fr>
2025-02-25 09:54:14 +0100

    Python -> Coq proof extraction

--- theories/entry_sample.v
+++ theories/entry_sample.v
@@ -12,22 +12,19 @@ Parameter cool : False.
    presentations. *)
 
 
-(* Presentation entry, in this case <a, b | babaabaa = abaaabaaa>. 
-
-In the current, unsatisfactory state, a presentation is just a rewrite system.
-Generators are inferred from the letters involved in the system.
-Later, we should also provide the list of generators. *)
-
-Definition present_entry := @Pres _ [::0; 1]  [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [::0;1;0;0;0;1;0;0;0])] erefl erefl.
+(* Presentation entry, in this case <a, b | babaabaa = abaaabaaa>. *)
+Definition present_entry := @Pres _ [::0; 1]
+    [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [::0; 1; 0; 0; 0; 1; 0; 0; 0])]
+    erefl erefl.
 
 (* Candidate alternate presentation, this time with three generators *)
-Definition present_final := @Pres _ [:: 0;1;2] [:: 
-                                    ([:: 1; 0; 0; 0], [:: 2]);
-                                    ([:: 1; 0; 1; 0; 0; 2], [:: 0; 2; 2; 0]);
-                                    ([:: 1; 0; 1; 0; 0; 1; 0; 0], [:: 0; 2; 2])] erefl erefl.
+Definition present_final := @Pres _ [:: 0; 1; 2]
+    [:: ([:: 1; 0; 0; 0], [:: 2]);
+        ([:: 1; 0; 1; 0; 0; 2], [:: 0; 2; 2; 0]);
+        ([:: 1; 0; 1; 0; 0; 1; 0; 0], [:: 0; 2; 2])] erefl erefl.
 
 
-(* Proof that the entry and final presentations define the same monoid. 
+(* Proof that the entry and final presentations define the same monoid.
    Warning: this is an effective result, containing the data of the isomorphism,
    hence the Defined. *)
 Section RewriteProofs.
@@ -35,17 +32,18 @@ Section RewriteProofs.
 Variables A : choiceType.
 
 Record cquad : Type := CQuad {
-cpre : word A;
-crel1 : word A;
-crel2 : word A;
-csuf : word A 
+  cpre : word A;
+  crel1 : word A;
+  crel2 : word A;
+  csuf : word A;
 }.
 
 Definition rew_cert := seq cquad.
 
-Definition wf_cquad (R : relat A) (c : cquad) := (crel1 c, crel2 c) \in undirected R.
+Definition wf_cquad (R : relat A) (c : cquad) :=
+  (crel1 c, crel2 c) \in undirected R.
 
-Definition cquad_rel (c1 c2 : cquad) := 
+Definition cquad_rel (c1 c2 : cquad) :=
   cpre c1 ++ crel2 c1 ++ csuf c1 == cpre c2 ++ crel1 c2 ++ csuf c2.
 
 Definition wf_cert (R : relat A) (c : cquad) (prf : rew_cert):=
@@ -61,12 +59,12 @@ Lemma check_certP (R : relat A) (u v : word A) (prf : rew_cert) :
   check_cert R u v prf -> u = v %[mod R].
 Proof.
 elim: prf u => [| c prf ihprf] u /=.
-- case/andP=> /= _; rewrite andbT => /eqP /=; rewrite !cats0 => ->. 
+- case/andP=> /= _; rewrite andbT => /eqP /=; rewrite !cats0 => ->.
   exact: rewrites_to_refl.
 case/andP=> /= /andP[wfc hall] /andP[] /eqP /=; rewrite cats0 => e hwf.
 pose u1 := cpre c ++ crel2 c ++ csuf c.
 have t1 : u = u1 %[mod R].
-  rewrite e /u1. apply: rewrites_to_stable; apply: rewrites_to1. 
+  rewrite e /u1. apply: rewrites_to_stable; apply: rewrites_to1.
   exact: rewrites_rel.
 apply: rewrites_to_trans t1 _.
 apply: ihprf; rewrite /check_cert hall /=.
@@ -82,35 +80,37 @@ End RewriteProofs.
 Theorem present_equiv :  isopres present_entry present_final.
 Proof.
 pose p0 := present_entry.
-pose p1 := @Pres _ [::0; 1; 2]  
-                 [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [::0;1;0;0;0;1;0;0;0]); 
-                 ([:: 2], [:: 1; 0; 0; 0])] erefl erefl.
+pose p1 := @Pres _ [::0; 1; 2]
+  [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [:: 0; 1; 0; 0; 0; 1; 0; 0; 0]);
+      ([:: 2], [:: 1; 0; 0; 0])] erefl erefl.
 have step_0 : isopres p0 p1.
   exact: (@Tietze_add_gen _ _ _ 2 [:: 1; 0; 0; 0]).
 apply: isopres_trans step_0 _.
-pose p2 := @Pres _ [::0; 1; 2]  
-   [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [::0;1;0;0;0;1;0;0;0]); 
-   ([:: 2], [:: 1; 0; 0; 0]); 
-   ([:: 1; 0; 1; 0; 0; 2], [:: 0; 2; 2; 0])] erefl erefl.
+pose p2 := @Pres _ [:: 0; 1; 2]
+  [:: ([:: 1; 0; 1; 0; 0; 1; 0; 0], [:: 0; 1; 0; 0; 0; 1; 0; 0; 0]);
+      ([:: 2], [:: 1; 0; 0; 0]);
+      ([:: 1; 0; 1; 0; 0; 2], [:: 0; 2; 2; 0])] erefl erefl.
 have step_1 : isopres p1 p2.
   apply: (@Tietze_add_rel _ _ _ [:: 1; 0; 1; 0; 0; 2] [:: 0; 2; 2; 0]) => //.
-  pose prf : rew_cert nat := [:: CQuad [:: 1; 0; 1; 0; 0] [:: 2] [:: 1; 0; 0; 0] [:: ];
-  CQuad [:: ] [:: 1; 0; 1; 0; 0; 1; 0; 0] [:: 0; 1; 0; 0; 0; 1; 0; 0; 0] [:: 0];
-  CQuad [:: 0] [:: 1; 0; 0; 0] [:: 2] [:: 1; 0; 0; 0; 0];
-  CQuad [:: 0; 2] [:: 1; 0; 0; 0] [:: 2] [:: 0]].
+  pose prf : rew_cert nat :=
+    [:: CQuad [:: 1; 0; 1; 0; 0] [:: 2] [:: 1; 0; 0; 0] [:: ];
+        CQuad [:: ] [:: 1; 0; 1; 0; 0; 1; 0; 0] [:: 0; 1; 0; 0; 0; 1; 0; 0; 0] [:: 0];
+        CQuad [:: 0] [:: 1; 0; 0; 0] [:: 2] [:: 1; 0; 0; 0; 0];
+        CQuad [:: 0; 2] [:: 1; 0; 0; 0] [:: 2] [:: 0]].
   exact: (check_certP (prf := prf)).
-  compute.
+apply: isopres_trans step_1 _.
 
-pose p3 := @Pres _ [::0; 1; 2]  
-[::  
-([:: 2], [:: 1; 0; 0; 0]); 
+
+pose p3 := @Pres _ [::0; 1; 2]
+[::
+([:: 2], [:: 1; 0; 0; 0]);
 ([:: 1; 0; 1; 0; 0; 2], [:: 0; 2; 2; 0])] erefl erefl.
 have step_1 : isopres p2 p3.
 Admitted.
 (* Step 0*)
 
 
-Corollary equiv_equal u v : 
+Corollary equiv_equal u v :
 (u = v %[mod present_entry]) <-> (present_equiv u = present_equiv v %[mod present_final_1]).
 Proof. admit. Admitted.
 
@@ -120,12 +120,12 @@ Proof. exact: (check_convergence_natP (fuel := 5)). Qed.
 
 (* The word problem is hence decidable in this monoid. *)
 Theorem decidable_entry : True.
-Proof. done. Qed. 
+Proof. done. Qed.
 
 (* To be implemented combining norfuel and termination somehow *)
 Parameter normalize : seq nat -> seq nat.
 
-Definition test_eq_entry u v := 
+Definition test_eq_entry u v :=
     (normalize (present_equiv u)) == normalize (present_equiv v).
 
 (* And now we provide a formally verified equality test for this monoid. *)
@@ -137,11 +137,11 @@ Proof. admit. Admitted.
  *)
 
 
- (* And now an optimized variant: implement a test_eq_opt and prove that it is 
+ (* And now an optimized variant: implement a test_eq_opt and prove that it is
    pointwise equal to tes_eq *)
 
 
-(***** END ******)   
+(***** END ******)
 
 Goal ([:: 1; 2; 2] < [:: 2; 2; 1])%O. by []. Qed.
 Goal ~~ ([:: 2; 2] < [:: 1])%O. by []. Qed.
