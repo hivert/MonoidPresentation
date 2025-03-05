d3920f36661aed42d52ba4e02bfdd7925eec27b4
Florent Hivert <Florent.Hivert@lri.fr>
2025-03-05 18:13:43 +0100

    Experiments with rewrites on strings

--- theories/stringmonoid.v
+++ theories/stringmonoid.v
@@ -4,7 +4,7 @@ From Coq Require Import Znat BinIntDef Uint63.
 From Coq Require Import PrimInt63 PString.
 
 
-Require Import monoids.
+Require Import monoids present.
 
 Set Implicit Arguments.
 Unset Strict Implicit.
@@ -23,6 +23,7 @@ HB.instance Definition _ := hasDecEq.Build int natint_eq_axiom.
 Lemma le0Z x : BinInt.Z.le 0 φ(x).
 Proof. by have [] := to_Z_bounded x. Qed.
 
+(* to_nat and of_nat are notation and not function ...*)
 Definition int_to_nat (i : int) : nat := to_nat i.
 Definition nat_to_int (n : nat) : int := of_nat n.
 Fact int_to_natK : cancel int_to_nat nat_to_int.
@@ -90,7 +91,49 @@ Proof. by move=> s t u; rewrite cat_assoc. Qed.
 HB.instance Definition _ := isMonoid.Build string
                               strcat_assoc cat_empty_l cat_empty_r.
 
-Check string : monoidType.
+Lemma lengthE s : to_nat (length s) = seq.size (to_list s).
+Proof. by rewrite length_spec. Qed.
+
+Lemma firstnE T n (l : list T) : List.firstn n l = take n l.
+Proof. by elim: n l => [|n IHn] [|l0 l] //=; rewrite IHn. Qed.
+Lemma skipnE T n (l : list T) : List.skipn n l = drop n l.
+Proof. by elim: n l => [|n IHn] [|l0 l] //=; rewrite IHn. Qed.
+
+Lemma take_strE s i : to_list (sub s 0 i) = take (to_nat i) (to_list s).
+Proof. by rewrite sub_spec List.skipn_O firstnE. Qed.
+
+Definition str_prefix u v := sub v 0 (length u) == u.
+
+Lemma str_prefixE u v : str_prefix u v = prefix (to_list u) (to_list v).
+Proof.
+by rewrite /str_prefix -(eqtype.inj_eq to_list_inj) take_strE lengthE prefixE.
+Qed.
+
+Fixpoint str_rewrites1_front R u :=
+  if R is (r1, r2) :: R' then
+    if str_prefix r1 u then Some (cat r2 (sub u (length r1) (length u)))
+    else str_rewrites1_front R' u
+  else None.
+Lemma str_rewrites1_frontE (R : (seq (string * string))) u :
+  (all (fun r => (length r.2 <= length r.1)%O) R) ->
+  omap to_list (str_rewrites1_front R u) =
+    rewrites1_front [seq (to_list r.1, to_list r.2) | r <- R] (to_list u).
+Proof.
+elim: R => [| [r1 r2] R IHR] //= /andP[lenr] {}/IHR <-.
+rewrite str_prefixE; case Hpref: prefix => //=.
+rewrite cat_spec; congr Some.
+rewrite sub_spec !firstnE !skipnE (lengthE r1).
+have lesz : seq.size (drop (seq.size (to_list r1)) (to_list u)) <= to_nat (length u).
+  by rewrite size_drop lengthE leq_subr.
+rewrite !take_oversize // {lesz} size_cat size_drop.
+have /lebP := valid_length u.
+rewrite leintbE /int_to_nat => /(leq_trans _); apply.
+move: lenr; rewrite leintE /int_to_nat !lengthE.
+move/size_prefix/subnKC : Hpref => {2}<-.
+by rewrite leq_add2r.
+Qed.
+
+(* Check string : monoidType.
 
 Eval compute in ("aaaa"%pstring * 1 * "bbb"%pstring)%M.
 
@@ -115,4 +158,4 @@ Time Eval native_compute in count (fun lp => lp.1 == lp.2)
                               [seq (p ++ q, q ++ p) | p <- lint, q <- lint].
 Time Eval native_compute in count (fun lp => lp.1 == lp.2)
                               [seq (cat p q, cat q p) | p <- lstr, q <- lstr].
-
+*)
