44a2c717a174d615f1a6da3c5609ce3e97ef629d
Florent Hivert <Florent.Hivert@lri.fr>
2025-04-06 10:25:09 +0200

    Fixed header wrt wfsizelexi

--- theories/certs/header.v
+++ theories/certs/header.v
@@ -3,7 +3,7 @@ From Coq Require Import (* Znat BinIntDef *) Uint63.
 Local Open Scope uint63_scope.
 
 Set Implicit Arguments.
-Unset Strict Implicit.
+Unset Strict Implicit. 
 Unset Printing Implicit Defensive.
 
-Require Import int_seq present rewcert fastcert criteria batchchecker.
+Require Import int_seq present rewcert fastcert criteria batchchecker wfsizelexi.
