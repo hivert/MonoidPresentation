077f279c632d296e8cfc58b9a45279a3e1df1e84
Florent Hivert <Florent.Hivert@lri.fr>
2025-06-25 19:08:59 +0200

    Greedy factorisation theory

--- theories/factor.v
+++ theories/factor.v
@@ -36,6 +36,13 @@ rewrite !size_cat addnC -[X in _ == X]addn0 -addnA eqn_add2l addn_eq0.
 by case/andP => /nilP -> /nilP ->.
 Qed.
 
+Lemma shape_take (T : Type) (s : seq (seq T)) i :
+  shape (take i s) = take i (shape s).
+Proof. by elim: i s => [| i IHi] [|s0 s] //= /[!IHi]. Qed.
+Lemma shape_drop (T : Type) (s : seq (seq T)) i :
+  shape (drop i s) = drop i (shape s).
+Proof. by elim: i s => [| i IHi] [|s0 s] /=. Qed.
+
 
 Section LongestPrefix.
 
@@ -290,22 +297,281 @@ Qed.
 End Infixes.
 
 
+Section DropSS.
+
+Context {Alph : eqType}.
+Implicit Type (s : seq (seq Alph)).
+
+Fixpoint dropss n s :=
+  if s is s0 :: s' then
+    if n < size s0 then (drop n s0) :: s'
+    else dropss (n - size s0) s'
+  else [::].
+
+Lemma dropss0 s : [::] \notin s -> dropss 0 s = s.
+Proof. by case: s => [|[|a s0] s]. Qed.
+
+Lemma nil_notin_dropss i s : [::] \notin s -> [::] \notin dropss i s.
+Proof.
+elim: s i => [|s0 s IHs] //= i; rewrite inE negb_or eq_sym => /andP[s0n0 snon0].
+case: ltnP => [lti | gei]; last exact: IHs.
+rewrite inE negb_or eq_sym snon0 andbT.
+apply/negP => /eqP/(congr1 size)/eqP.
+by rewrite size_drop /= subn_eq0 leqNgt lti.
+Qed.
+
+Lemma flatten_dropss i s : flatten (dropss i s) = drop i (flatten s).
+Proof. by elim: s i => [|s0 s IHs] //= i; rewrite drop_cat; case: ltnP. Qed.
+
+Lemma size_dropss i s : size (dropss i s) <= size s.
+Proof.
+elim: s i => [|s0 s IHs] //= i.
+case: ltnP => [lti | gei] //.
+by apply: ltnW; rewrite ltnS; exact: IHs.
+Qed.
+
+End DropSS.
+
+
 Section GreedyFactorisation.
 
 Context {Alph : choiceType}.
 
 Implicit Type (u v w : word Alph).
-Implicit Type (P : pres Alph).
+Variable p : pred (word Alph).
+Hypothesis pnil : p [::].
+Hypothesis pstable : forall u v, p u -> infix v u -> p v.
+
+Lemma all_dropss g n : all p g -> all p (dropss n g).
+Proof.
+move/allP => /= allpg; apply/allP => /=.
+elim: g n allpg => // g0 g IHg n pg => v /=.
+have pg0 : p g0 by apply: pg; rewrite inE eqxx.
+have {}pg : {in g, forall x : seq Alph, p x}.
+  by move=> /= w win; apply: pg; rewrite inE win orbT.
+case: ltnP => _; last exact: IHg.
+rewrite inE => /orP[/eqP-> | /pg //].
+exact/(pstable pg0)/infix_drop.
+Qed.
 
 (** u is a greedy prefix of v for the pieces accepted by p *)
-Definition is_greedy_prefix (p : pred (word Alph)) u v :=
-  (u == v) || prefix u v && ~~ p (take (size u).+1 v).
-Fixpoint is_greedy_rec (p : pred (word Alph)) f :=
+Definition is_greedy_prefix u v :=
+  p u && ((u == v) || prefix u v && ~~ p (take (size u).+1 v)).
+Fixpoint is_greedy_rec f :=
   if f is f0 :: tl then
-    if ~~ (is_greedy_prefix p f0 (f0 ++ head [::] tl)) then false
-    else is_greedy_rec p tl
+    (is_greedy_prefix f0 (f0 ++ head [::] tl)) && (is_greedy_rec tl)
   else true.
-Definition is_greedy_factorisation (p : pred (word Alph)) u f :=
-  ([::] \notin f) && (flatten f == u) && (is_greedy_rec p f).
+Definition is_greedy_factorisation u f :=
+  [&& [::] \notin f, (* to prevent factorisation ending with [::] *)
+    flatten f == u &
+      is_greedy_rec f].
+
+
+Lemma is_greedy_prefix_eq u v : is_greedy_prefix u v -> p v -> u = v.
+Proof.
+rewrite /is_greedy_prefix => /andP[pu /[swap] pv] /orP[/eqP -> // | /andP[pref]].
+by have /prefixW/(pstable pv) -> := prefix_take v (size u).+1.
+Qed.
+
+Lemma is_greedy_prefix_impl u v1 v2 :
+  take (size u).+1 v1 = take (size u).+1 v2 ->
+  is_greedy_prefix u v1 -> is_greedy_prefix u v2.
+Proof.
+rewrite /is_greedy_prefix => eqtake; case: (p u) => //=.
+case: eqP => [equv1 _ | /eqP nequv1 /andP[]] /=.
+  suff -> : u = v2 by rewrite eqxx.
+  move: eqtake; rewrite -{v1}equv1 take_oversize // => /[dup] equ /(congr1 size).
+  rewrite size_take; case: ltnP => [_ /eqP| _ eqsz]; first by rewrite ltn_eqF.
+  by rewrite equ eqsz take_oversize.
+rewrite !prefixE eqtake => /eqP {4}<- ->.
+rewrite andbT /=; apply/orP; right; apply/eqP.
+move/(congr1 (take (size u))): eqtake.
+by rewrite -!take_min; have /minn_idPl -> : size u <= (size u).+1.
+Qed.
+Lemma is_greedy_prefixE u v1 v2 :
+  take (size u).+1 v1 = take (size u).+1 v2 ->
+  is_greedy_prefix u v1 = is_greedy_prefix u v2.
+Proof. by move=> H; apply/idP/idP; apply: is_greedy_prefix_impl; rewrite H. Qed.
+
+Lemma is_greedy_prefix_take u v :
+  is_greedy_prefix u v = is_greedy_prefix u (take (size u).+1 v).
+Proof. by apply: is_greedy_prefixE; rewrite -take_min minnn. Qed.
+
+Theorem is_greedy_min_size u (f g : seq (word Alph)) :
+  is_greedy_factorisation u f -> flatten g = u -> all p g -> size f <= size g.
+Proof.
+move: f g; have [n leMn] := ubnP (size u); elim: n u leMn => // n IHn u.
+rewrite ltnS leq_eqVlt => /orP[/eqP szu f g|]; last exact: IHn.
+case: n IHn szu => [_ /eqP/nilP -> | n IHn szu].
+  rewrite /is_greedy_factorisation /=.
+  by case: f => [|[|h0 f0] f] //= /[!andbF].
+rewrite /is_greedy_factorisation; case eqf: f => [//| f0 ftl] /and3P[].
+rewrite -{2 4}eqf inE negb_or eq_sym => /andP[f0n0 fn0] /eqP flatf /=/andP[greed rec].
+case eqg : g => [/= | g0 gtl].
+  by move=> equ _; rewrite -equ /= in szu.
+rewrite -{1}eqg => flatg /[dup] allpg /= /andP[pg0 _].
+have {}greed : is_greedy_prefix f0 u.
+  rewrite -flatf {}eqf /=.
+  case: ftl fn0 flatf greed {rec} => [_ /eqP | f1 ftl] //=.
+  case: f1 => // a f1 _ _ //=; apply: is_greedy_prefix_impl.
+  by rewrite !take_cat ltnNge leqnSn /= subSnn /= !take0.
+case: (ltnP (size f0) (size g0)) => [lt0 | le0].
+  exfalso; have:= flatf; rewrite -flatg /= eqf eqg /= => /cat2E.
+  case/(_ (ltnW lt0)) => suff Hsuff eqg0.
+  have ptu : p (take (size f0).+1 u).
+    apply/(pstable pg0)/prefixW.
+    suff -> : take (size f0).+1 u = take (size f0).+1 g0 by apply: prefix_take.
+    by rewrite -flatg eqg /= takel_cat.
+  move: greed; rewrite /is_greedy_prefix {}ptu /= andbF orbF andbC.
+  case: eqP => // eqf0u _; subst f0.
+  by move: lt0; rewrite -flatg eqg /= size_cat ltnNge leq_addr.
+have {rec greed}/IHn greedftl : is_greedy_factorisation (flatten ftl) ftl.
+  by rewrite /is_greedy_factorisation /= fn0 eqxx rec /=.
+have {}/greedftl lesz : size (flatten ftl) < n.+1.
+  rewrite -szu -flatf eqf size_cat; move: f0n0; case f0 => // a0 f0' _.
+  by rewrite /= addSn ltnS leq_addl.
+rewrite eqf /= ltnS.
+case: ftl eqf flatf lesz fn0 => //= f1 ftl eqf flatf lesz.
+rewrite inE negb_or eq_sym => /andP[f1n0 _].
+case: gtl eqg flatg allpg => [-> | g1 gtl eqg flatg].
+  rewrite /= cats0 => eqg0 _; subst g0; exfalso.
+  move: flatf le0 => /(congr1 size) <-.
+  case: f1 {lesz} f1n0 eqf => // a f1 _ ->.
+  by rewrite !size_cat /= addSn addnS ltnNge leq_addr.
+rewrite -eqg => allpg.
+apply: (leq_trans (lesz (dropss (size f0) g) _ _)) => {lesz}.
+- by rewrite flatten_dropss flatg -flatf eqf /= drop_size_cat.
+- exact: all_dropss.
+- rewrite eqg /= ltnNge le0 /=; case: ltnP => _ //=.
+  exact: (leq_trans (size_dropss _ _)).
+Qed.
+
+
+Fixpoint greedy_prefsize_rec u n :=
+  if n is n0.+1 then if p (take n u) then n else greedy_prefsize_rec u n0
+  else 0.
+Definition greedy_prefsize u := greedy_prefsize_rec u (size u).
+
+Lemma greedy_prefsize_full u : p u -> greedy_prefsize u = size u.
+Proof.
+rewrite /greedy_prefsize.
+by case: u => // u0 u /=; rewrite take_size => ->.
+Qed.
+
+Lemma greedy_prefsize_rec_le u n : greedy_prefsize_rec u n <= n.
+Proof. by elim: n => //= n IHn; case: p => //; exact: (leq_trans IHn). Qed.
+Lemma greedy_prefsize_le u : greedy_prefsize u <= size u.
+Proof. exact: greedy_prefsize_rec_le. Qed.
+
+Lemma greedy_prefsizeP u :
+  is_greedy_prefix (take (greedy_prefsize u) u) u.
+Proof.
+rewrite /is_greedy_prefix.
+case: (boolP (p u)) => [/[dup] pu /greedy_prefsize_full -> | npu].
+  by rewrite take_size pu eqxx.
+rewrite /greedy_prefsize size_take.
+case Hsz : (size u) => [|n] /=.
+  by exfalso; move: Hsz npu => /eqP/nilP ->; rewrite pnil.
+have nptake1 : ~~ p (take n.+1 u) by rewrite -Hsz take_size.
+rewrite (negbTE nptake1) ltnS greedy_prefsize_rec_le.
+elim : n {Hsz} nptake1 => [| n IHn] /= nptake.
+  by rewrite take0 pnil prefix0s nptake orbC.
+case: (boolP (p (take n.+1 u))) => [ptake | /IHn //].
+by rewrite ptake nptake prefix_take orbC.
+Qed.
+
+Fixpoint greedy_factor_rec u fuel :=
+  if fuel is fuel'.+1 then
+    if u == [::] then Some [::] else
+      let prefsize := greedy_prefsize u in
+      if prefsize == 0 then None else
+        if greedy_factor_rec (drop prefsize u) fuel' is Some res
+        then Some ((take prefsize u) :: res)
+        else None
+  else None.
+Definition greedy_factor u := greedy_factor_rec u (size u).+1.
+
+Implicit Type (s : seq (word Alph)).
+
+Lemma greedy_factor_recP u fuel s :
+  size u < fuel -> greedy_factor_rec u fuel = Some s ->
+  is_greedy_factorisation u s.
+Proof.
+move=> + /eqP.
+elim: fuel u s => [| fuel IHfuel] u s //=; rewrite ltnS => szu.
+case: (altP (u =P [::])) => [-> /eqP[<-] // | uneq0].
+case: (altP (greedy_prefsize u =P 0)) => // grprfsz.
+have:= greedy_prefsize_le u; rewrite leq_eqVlt => /orP[/eqP /[dup] Hgr -> | Hgr].
+  rewrite drop_size; case: fuel szu {IHfuel} => //= fuel _ /eqP[{s}<-].
+  have:= greedy_prefsizeP u; rewrite Hgr take_size.
+  by rewrite /is_greedy_factorisation /= cats0 inE eq_sym uneq0 eqxx andbT /=.
+have {}/IHfuel : size (drop (greedy_prefsize u) u) < fuel.
+  apply: (leq_trans _ szu); rewrite size_drop ltn_subrL.
+  by rewrite lt0n grprfsz /=; apply: (leq_ltn_trans _ Hgr).
+case: (greedy_factor_rec _ _) => // => fact /(_ fact (eqxx _)) Hfact /eqP[{s}<-].
+move: Hfact; rewrite /is_greedy_factorisation /=.
+rewrite inE negb_or => /and3P[/[dup] fact0 -> /eqP/[dup] Hfact-> ->].
+rewrite cat_take_drop eqxx !andbT andbC /= andbC eq_sym; apply/andP; split.
+  case: u uneq0 grprfsz {szu Hgr fact fact0 Hfact} => [|u0 u] // _.
+  by case: (greedy_prefsize _).
+have {grprfsz} := greedy_prefsizeP u; apply: is_greedy_prefix_impl.
+rewrite size_take Hgr.
+rewrite -{2}(cat_take_drop (greedy_prefsize u) u) -{}Hfact.
+case: fact fact0 => [//| f0 fact] /= Hfact.
+rewrite take_cat [in RHS]take_cat size_take Hgr subSnn ltnNge leqnSn /=.
+move: Hfact; rewrite inE negb_or eq_sym => /andP[/[swap] _].
+by case: f0 => // a f1 _ /=; rewrite !take0.
+Qed.
+Lemma greedy_factorP u s :
+  greedy_factor u = Some s -> is_greedy_factorisation u s.
+Proof. exact: greedy_factor_recP. Qed.
+
+
+Lemma greedy_factor_recNP u fuel :
+  size u < fuel ->
+  forall s, all p s -> flatten s = u -> greedy_factor_rec u fuel != None.
+Proof.
+elim: fuel u => [| fuel IHfuel] u //=; rewrite ltnS => szu s /allP allps flats.
+have {allps} : all p [seq v <- s | v != [::]].
+  by apply/allP => v /[!mem_filter]/andP[_ /allps].
+have {flats} : flatten [seq v <- s | v != [::]] = u.
+  rewrite -{}flats; elim: s => // s0 s IHs /=.
+  by case: eqP => [-> //= | _ /=]; rewrite IHs.
+have : [::] \notin [seq v <- s | v != [::]] by rewrite mem_filter eqxx.
+case: (filter _ s) => [|[|a s0] {}s] // sn0 flats allps.
+  by rewrite -flats eqxx.
+rewrite /= in flats; rewrite -{1}flats /=.
+case: (altP (greedy_prefsize u =P 0)) => [gr0 | grprfsz].
+  exfalso.
+  have {allps} pa : p [:: a].
+    move: allps => /andP[/pstable Hstable].
+    by have {}/Hstable : infix [:: a] (a :: s0)
+      by apply: prefixW; rewrite /= eqxx prefix0s.
+  have:= greedy_prefsizeP u; rewrite gr0 take0 /is_greedy_prefix.
+  by rewrite pnil /= -flats /= take0 pa.
+have {}/IHfuel : size (drop (greedy_prefsize u) u) < fuel.
+  apply: (leq_trans _ szu); rewrite size_drop ltn_subrL.
+  by rewrite lt0n grprfsz /= -flats.
+case: (greedy_factor_rec _ _) => //.
+move/(_ (dropss (greedy_prefsize u) ((a :: s0) :: s))); apply; first last.
+  by rewrite flatten_dropss /= flats.
+exact: all_dropss.
+Qed.
+Lemma greedy_factorNP u :
+  forall s, all p s -> flatten s = u -> greedy_factor u != None.
+Proof. exact: greedy_factor_recNP. Qed.
 
 End GreedyFactorisation.
+
+
+Section Bla.
+
+Let G := [:: [:: 1; 2]; [:: 3; 4; 5]; [:: 6; 7]].
+Let f0 := [:: 1; 2; 3].
+
+Goal is_greedy_factorisation predT [:: 1; 2] [:: [:: 1;  2]].
+by compute. Qed.
+Goal is_greedy_factorisation (fun v => size v <= 1) [:: 1; 2] [:: [:: 1];  [:: 2]].
+by compute. Qed.
+
+End Bla.
