4023ac02ee2697e6fa1d8937356685241d47839d
Florent Hivert <florent.hivert@lisn.fr>
2025-08-10 23:31:37 +0200

    Added missing header_dec.v header_pres.v

---
+++ theories/samples/header_pres.v
@@ -0,0 +1,10 @@
+From Coq Require Import (* Znat BinIntDef *) Uint63.
+From mathcomp Require Import ssreflect ssrbool ssrfun seq choice.
+
+Local Open Scope uint63_scope.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Require Import int_seq present.
