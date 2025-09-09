4cba9daec92ae395cae028677eaf8821e9b71f23
Florent Hivert <florent.hivert@lisn.fr>
2025-09-09 17:40:25 +0200

    Cleanup in sample + header

---
+++ theories/database_dec.v
@@ -0,0 +1,7 @@
+(* This file is imported by all the WP decision files of the database *)
+From Coq Require Export Uint63.
+From mathcomp Require Export ssreflect ssrbool ssrfun seq choice.
+Require Export int_seq present rewcert fastcert criteria batchchecker sizelexi.
+
+Open Scope uint63_scope.
+
---
+++ theories/database_pres.v
@@ -0,0 +1,7 @@
+(* This file is imported by all the presentation files of the database *)
+From Coq Require Export Uint63.
+From mathcomp Require Export ssreflect seq.
+Require Export int_seq present.
+
+Open Scope uint63_scope.
+
