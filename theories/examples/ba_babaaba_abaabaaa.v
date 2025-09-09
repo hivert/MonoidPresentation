f8179b3a2f2556888ba0b98529a32784bafe112c
Florent Hivert <florent.hivert@lisn.fr>
2025-09-09 21:20:59 +0200

    Better extraction example thanks to \\in computation

--- theories/examples/ba_babaaba_abaabaaa.v
+++ theories/examples/ba_babaaba_abaabaaa.v
@@ -7,9 +7,8 @@ Definition P :=
 
 Theorem P_dec : WPdecidable P.
 Proof.
-have : (onth_int CompleteRewritingSystem_batch013_pres.all_pres 369) = Some P.
-  exact/eqP.
-by move/onth_int_mem/CompleteRewritingSystem_batch013_dec.all_pres_dec.
+have : P \in CompleteRewritingSystem_batch013_pres.all_pres by [].
+exact: CompleteRewritingSystem_batch013_dec.all_pres_dec.
 Qed.
 
 
