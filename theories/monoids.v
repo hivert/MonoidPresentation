3ffd4ef94cd2e3291caaebc8723a96f0fb976ac0
Florent Hivert <florent.hivert@lisn.fr>
2025-12-02 10:49:02 +0100

    Monoid complement to be contributed to mathcomp

--- theories/monoids.v
+++ theories/monoids.v
@@ -14,8 +14,7 @@
 (*                  http://www.gnu.org/licenses/                              *)
 (******************************************************************************)
 From HB Require Import structures.
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat seq.
-From mathcomp Require Import choice bigop fintype finfun finset ssralg monoid.
+From mathcomp Require Import all_boot ssralg.
 
 
 Set Implicit Arguments.
@@ -35,7 +34,14 @@ Reserved Notation "{ 'transf' T }" (at level 0, format "{ 'transf'  T }").
 Reserved Notation "{ 'ptransf' T }" (at level 0, format "{ 'ptransf'  T }").
 Reserved Notation "{ 'relat' T }" (at level 0, format "{ 'relat'  T }").
 
-(* Commutative monoids *)
+
+Notation "{ 'mmorphism' U -> V }" := (UMagmaMorphism.type U%type V%type)
+  : type_scope.
+
+
+(* TODO : contribute to MathComp *)
+
+(* Commutative magma/semigroups/monoids *)
 HB.mixin Record Magma_hasCommutativeMul M of Magma M := {
   mulgC : commutative (@mul M)
 }.
@@ -43,13 +49,57 @@ HB.mixin Record Magma_hasCommutativeMul M of Magma M := {
 HB.structure Definition ComMagma :=
   {M of Magma M & Magma_hasCommutativeMul M}.
 
+HB.structure Definition ComChoiceMagma :=
+  {M of ChoiceMagma M & Magma_hasCommutativeMul M}.
+#[short(type="subComMagmaType")]
+HB.structure Definition SubComMagma (R : magmaType) S :=
+  {U of SubMagma R S U & Magma_hasCommutativeMul U}.
+
+#[short(type="comSemigroupType")]
+HB.structure Definition ComSemigroup :=
+  {M of Semigroup M & Magma_hasCommutativeMul M}.
+#[short(type="subComSemigroupType")]
+HB.structure Definition SubComSemigroup (R : semigroupType) S :=
+  {U of SubSemigroup R S U & Magma_hasCommutativeMul U}.
+
+
+#[short(type="comBaseUMagmaType")]
+HB.structure Definition ComBaseUMagma :=
+  {M of BaseUMagma M & Magma_hasCommutativeMul M}.
+
+HB.structure Definition ComChoiceBaseUMagma :=
+  {M of ChoiceBaseUMagma M & Magma_hasCommutativeMul M}.
+#[short(type="subComBaseUMagmaType")]
+HB.structure Definition SubComBaseUMagma (R : umagmaType) S :=
+  {U of SubBaseUMagma R S U & Magma_hasCommutativeMul U}.
+
 #[short(type="comUMagmaType")]
 HB.structure Definition ComUMagma :=
   {M of UMagma M & Magma_hasCommutativeMul M}.
+#[short(type="subComUMagmaType")]
+HB.structure Definition SubComUMagma (R : umagmaType) S :=
+  {U of SubUMagma R S U & ComChoiceMagma U}.
 
 #[short(type="comMonoidType")]
 HB.structure Definition ComMonoid :=
   {M of Monoid M & Magma_hasCommutativeMul M}.
+#[short(type="subComMonoidType")]
+HB.structure Definition SubComMonoid (R : monoidType) S :=
+  {U of SubMonoid R S U & ComMonoid U}.
+
+#[short(type="comBaseGroupType")]
+HB.structure Definition ComBaseGroup :=
+  {M of BaseGroup M & Magma_hasCommutativeMul M}.
+#[short(type="comStarMonoidType")]
+HB.structure Definition ComStarMonoid :=
+  {M of StarMonoid M & Magma_hasCommutativeMul M}.
+
+#[short(type="comGroupType")]
+HB.structure Definition ComGroup :=
+  {M of Group M & Magma_hasCommutativeMul M}.
+#[short(type="subComGroupType")]
+HB.structure Definition SubComGroup (R : groupType) S :=
+  {U of SubGroup R S U & ComBaseGroup U}.
 
 
 HB.factory Record isComMonoid V & Choice V := {
@@ -67,61 +117,134 @@ HB.instance Definition _ := Magma_hasCommutativeMul.Build V mulgC.
 HB.end.
 
 
-Notation "{ 'mmorphism' U -> V }" := (UMagmaMorphism.type U%type V%type)
-  : type_scope.
-
-(*
-Notation mmorph1 := gmulf1.
-Notation mmorphM := gmulfM.
-Notation mmorph_prod := gmulf_prod.
-Notation mmorphXn := gmulfXn.
-*)
-
-(* Sub commutative monoids *)
-#[short(type="subComMonoidType")]
-HB.structure Definition SubComMonoid (R : monoidType) S :=
-  {U of SubMonoid R S U & ComMonoid U}.
-
-HB.factory Record SubMonoid_isSubComMonoid (R : comMonoidType) S U
-    of SubMonoid R S U := {}.
-
-HB.builders Context R S U of SubMonoid_isSubComMonoid R S U.
+HB.factory Record SubMagma_isSubComMagma (R : comMagmaType) S U
+    of SubMagma R S U := {}.
+HB.builders Context R S U of SubMagma_isSubComMagma R S U.
 Lemma mulgC : @commutative U U *%g.
 Proof. by move=> x y; apply: val_inj; rewrite !gmulfM mulgC. Qed.
 HB.instance Definition _ := Magma_hasCommutativeMul.Build U mulgC.
 HB.end.
 
+HB.factory Record SubSemigroup_isSubComSemigroup (R : comSemigroupType) S U
+    of SubSemigroup R S U := {}.
+HB.builders Context R S U of SubSemigroup_isSubComSemigroup R S U.
+HB.instance Definition _ := SubMagma_isSubComMagma.Build R S U.
+HB.end.
+
+HB.factory Record SubUMagma_isSubComUMagma (R : comUMagmaType) S U
+    of SubUMagma R S U := {}.
+HB.builders Context R S U of SubUMagma_isSubComUMagma R S U.
+HB.instance Definition _ := SubMagma_isSubComMagma.Build R S U.
+HB.end.
+
+HB.factory Record SubMonoid_isSubComMonoid (R : comMonoidType) S U
+    of SubMonoid R S U := {}.
+HB.builders Context R S U of SubMonoid_isSubComMonoid R S U.
+HB.instance Definition _ := SubMagma_isSubComMagma.Build R S U.
+HB.end.
+
+
+
+(* Converse M* *)
+Section Converse.
+
+HB.instance Definition _ (R : magmaType) :=
+  let mul' (x y : R) := y * x in hasMul.Build R^c mul'.
+HB.instance Definition _ (R : ChoiceMagma.type) := ChoiceMagma.on R^c.
+HB.instance Definition _ (R : semigroupType) :=
+  let mulgA' x y z := esym (mulgA z y x) in
+  Magma_isSemigroup.Build R^c mulgA'.
+HB.instance Definition _ (R : baseUMagmaType) := hasOne.Build R^c 1.
+HB.instance Definition _ (R : umagmaType) :=
+  BaseUMagma_isUMagma.Build R^c mulg1 mul1g.
+HB.instance Definition _ (R : monoidType) := Monoid.on R^c.
+
+End Converse.
+
+(* FIXME: HB.saturate *)
+HB.instance Definition _ (R : comMagmaType) :=
+  Magma_hasCommutativeMul.Build R^c (fun _ _ => mulgC _ _).
+HB.instance Definition _ (R : ComChoiceMagma.type) := ComChoiceMagma.on R^c.
+HB.instance Definition _ (R : comSemigroupType) := ComSemigroup.on R^c.
+HB.instance Definition _ (R : comBaseUMagmaType) := ComBaseUMagma.on R^c.
+HB.instance Definition _ (R : comUMagmaType) := ComUMagma.on R^c.
+HB.instance Definition _ (R : comMonoidType) := ComMonoid.on R^c.
+(* /FIXME *)
+
+
+Section FinFunMagma.
+Variable (aT : finType).
+
+Lemma ffun_mulC  (rT : comMagmaType) : commutative (@mul {ffun aT -> rT}).
+Proof. by move=> f1 f2; apply/ffunP=> a; rewrite !ffunE mulgC. Qed.
+
+HB.instance Definition _  (rT : comMagmaType) :=
+  Magma_hasCommutativeMul.Build {ffun aT -> rT} (@ffun_mulC rT).
+
+
+(* FIXME: HB.saturate *)
+HB.instance Definition _ (rT : ComChoiceMagma.type) := ChoiceMagma.on {ffun aT -> rT}.
+HB.instance Definition _ (rT : comSemigroupType) := Semigroup.on {ffun aT -> rT}.
+HB.instance Definition _ (rT : comBaseUMagmaType) := BaseUMagma.on {ffun aT -> rT}.
+HB.instance Definition _ (rT : comUMagmaType) := UMagma.on {ffun aT -> rT}.
+HB.instance Definition _ (rT : comMonoidType) := Monoid.on {ffun aT -> rT}.
+(* /FIXME *)
+
+End FinFunMagma.
+
+
+(* A product of commutative m* is a canonical instance of comM* *)
+Section ComProduct.
+
+Variable (U V : comMagmaType).
+Fact pair_mulC : @commutative (U * V) (U * V) *%g.
+Proof. by move=> [x1 y1][x2 y2]; congr (_, _); rewrite mulgC. Qed.
+HB.instance Definition _ :=
+  Magma_hasCommutativeMul.Build (U * V)%type pair_mulC.
+
+End ComProduct.
+
+(* FIXME: HB.saturate *)
+HB.instance Definition _ (U V : ComChoiceMagma.type) := ChoiceMagma.on (U * V)%type.
+HB.instance Definition _ (U V : comSemigroupType) := Semigroup.on (U * V)%type.
+HB.instance Definition _ (U V : comBaseUMagmaType) := BaseUMagma.on (U * V)%type.
+HB.instance Definition _ (U V : comUMagmaType) := UMagma.on (U * V)%type.
+HB.instance Definition _ (U V : comMonoidType) := Monoid.on (U * V)%type.
+(* /FIXME *)
+
+
+
 (* Monoid structure on (seq T) for T a choiceType, aka the free monoid *)
-Definition FreeMonoidT (a : choiceType) := seq a.
-HB.instance Definition _ (a : choiceType) := Choice.on (FreeMonoidT a).
-HB.instance Definition _ (a : choiceType) :=
-  isMonoid.Build (FreeMonoidT a) (@catA a) (@cat0s a) (@cats0 a).
+Definition FreeMonoidT (T : choiceType) := seq T.
+HB.instance Definition _ (T : choiceType) := Choice.on (FreeMonoidT T).
+HB.instance Definition _ (T : choiceType) :=
+  isMonoid.Build (FreeMonoidT T) (@catA T) (@cat0s T) (@cats0 T).
 
 Notation "{ 'freemon' T }" := (FreeMonoidT T).
 Notation "[fmon x ]" := ([:: x] : {freemon _}).
 
-Lemma FreeMonoidE (a : choiceType) (x : {freemon a}) :
-  x = \prod_(i <- x) [fmon i].
+Lemma FreeMonoidE (T : choiceType) (m : {freemon T}) :
+  m = \prod_(i <- m) [fmon i].
 Proof.
-by elim: x => [|s s0 {1}->]; rewrite ?big_nil // big_cons [RHS]cat1s.
+by elim: m => [|s s0 {1}->]; rewrite ?big_nil // big_cons [RHS]cat1s.
 Qed.
 
 (* Universal property for the free monoid *)
 Section UniversalProperty.
 
-Variables (A : choiceType) (M : monoidType) (f : A -> M).
+Variables (T : choiceType) (M : monoidType) (f : T -> M).
 
-Definition univmor (m : {freemon A}) : M := \prod_(i <- m) f i.
+Definition univmor (m : {freemon T}) : M := \prod_(i <- m) f i.
 
 Lemma univmor_is_monoid_morphism : monoid_morphism univmor.
 Proof. rewrite /univmor; by split => [|x y]; rewrite -?big_cat ?big_nil. Qed.
 #[export]
-HB.instance Definition _ := isUMagmaMorphism.Build {freemon A} M univmor
+HB.instance Definition _ := isUMagmaMorphism.Build {freemon T} M univmor
   univmor_is_monoid_morphism.
 Lemma univmorE x : univmor [fmon x] = f x.
 Proof. by rewrite /univmor big_seq1. Qed.
-Lemma univmor_uniq (g : {mmorphism {freemon A} -> M}) :
-  (forall a : A, g [fmon a] = f a) -> g =1 univmor.
+Lemma univmor_uniq (g : {mmorphism {freemon T} -> M}) :
+  (forall a : T, g [fmon a] = f a) -> g =1 univmor.
 Proof.
 move=> eq m; rewrite (FreeMonoidE m) !gmulf_prod; apply: eq_bigr => i _ {m} /=.
 by rewrite eq univmorE.
@@ -145,9 +268,9 @@ Qed.
 
 End UniversalProperty.
 
-Lemma mmorph_univmorE (A : choiceType) (M : monoidType)
-  (f : {mmorphism {freemon A} -> M}) :
-  f =1 univmor (fun a : A => f [fmon a]).
+Lemma mmorph_univmorE (T : choiceType) (M : monoidType)
+  (f : {mmorphism {freemon T} -> M}) :
+  f =1 univmor (fun a : T => f [fmon a]).
 Proof.
 move=> x; rewrite (FreeMonoidE x).
 rewrite !gmulf_prod; apply: eq_bigr => a _ /=.
@@ -277,14 +400,21 @@ move=> f; apply/setP => [[x y]]; rewrite inE.
 apply/existsP/idP => [[z /= /andP[/[!onerelE] /eqP ->]] // | xy_in_f /=].
 by exists x; rewrite onerelE eqxx.
 Qed.
-Lemma mulrel1 : right_id onerel mulrel.
+Definition dualrel f : type := [set p | (p.2, p.1) \in f].
+Lemma dualrelE f x y : ((x, y) \in dualrel f) = ((y, x) \in f).
+Proof. by rewrite inE. Qed.
+Lemma dualrelK : involutive dualrel.
+Proof. by move=> f; apply/setP => [[x y]]; rewrite !dualrelE. Qed.
+Lemma dualrelM f g : dualrel (mulrel f g) = mulrel (dualrel g) (dualrel f).
 Proof.
-move=> f; apply/setP => [[x y]]; rewrite inE.
-apply/existsP/idP => [[z /andP[/[swap]/[!onerelE] /eqP ->]] // | xy_in_f /=].
-by exists y; rewrite onerelE eqxx andbT.
+apply/setP => [[x y]]; rewrite dualrelE.
+apply/mulrelP/mulrelP => [[z yz_in_f zx_in_g] | [z]].
+  by exists z; rewrite dualrelE.
+by rewrite !dualrelE => [zx_in_g yz_in_f]; exists z.
 Qed.
 #[export]
-HB.instance Definition _ := isMonoid.Build type mulrelA mul1rel mulrel1.
+HB.instance Definition _ :=
+  isStarMonoid.Build type mulrelA mul1rel dualrelK dualrelM.
 
 End Relation.
 
@@ -307,49 +437,4 @@ HB.export Relation.Exports.
 Notation "{ 'relat' T }" := (Relation.type T).
 
 
-Section RelatTheory.
-Variable (T : finType).
-Implicit Types (f g h : {relat T}).
 
-Definition dualrel f : {relat T} := [set p | (p.2, p.1) \in f].
-Lemma dualrelE f x y : ((x, y) \in dualrel f) = ((y, x) \in f).
-Proof. by rewrite inE. Qed.
-Lemma dualrelK : involutive dualrel.
-Proof. by move=> f; apply/setP => [[x y]]; rewrite !dualrelE. Qed.
-Lemma dualrel1 : dualrel 1 = 1 :> {relat T}.
-Proof. by apply/setP=> [[x y]]; rewrite dualrelE !onerelE eq_sym. Qed.
-Lemma dualrelM f g : dualrel (f * g) = dualrel g * dualrel f.
-Proof.
-apply/setP => [[x y]]; rewrite dualrelE.
-apply/mulrelP/mulrelP => [[z yz_in_f zx_in_g] | [z]].
-  by exists z; rewrite dualrelE.
-by rewrite !dualrelE => [zx_in_g yz_in_f]; exists z.
-Qed.
-End RelatTheory.
-
-
-
-(* Converse monoid *)
-Section ConverseMonoid.
-
-HB.instance Definition _ (R : monoidType) :=
-  let mul' (x y : R) := (y * x) in
-  let mulgA' x y z := esym (mulgA z y x) in
-  isMonoid.Build R^c (mulgA' : associative mul') mulg1 mul1g.
-
-HB.instance Definition _ (R : comMonoidType) :=
-  Magma_hasCommutativeMul.Build R^c (fun _ _ => mulgC _ _).
-
-End ConverseMonoid.
-
-
-(* A product of commutative monoid is a canonical instance of comoid *)
-Section ComProduct.
-
-Variable (U V : comMonoidType).
-Fact pair_mulC : @commutative (U * V) (U * V) ( *%g ).
-Proof. by move=> [x1 y1][x2 y2]; congr (_, _); rewrite mulgC. Qed.
-HB.instance Definition _ :=
-  Magma_hasCommutativeMul.Build (U * V)%type pair_mulC.
-
-End ComProduct.
